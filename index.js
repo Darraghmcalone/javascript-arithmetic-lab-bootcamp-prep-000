0d31a7d15751c9a1732b6703c9c9e0d13e9e21e1
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-23 10:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var number = 6
+
+var number = 10
+ 
 function add5() {
   number += 5
 }
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number) 
+ 
+add5()
+ 
 console.log(number)
